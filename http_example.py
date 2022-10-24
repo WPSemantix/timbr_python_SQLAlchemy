aca5cfdd5c95b25e8a84210353d3bc330974270c
Amit Weitzner <42403724+amitwps@users.noreply.github.com>
2022-10-24 21:55:39 +0300

    Update http_example.py

--- http_example.py
+++ http_example.py
@@ -6,7 +6,7 @@ def executeQuery(url, ontology, token, query):
        url += "/"   
     post_data = {'ontology_name': ontology, 'query': query}
     headers = {'Content-Type': 'application/json', 'x-api-key': token}
-    response = requests.post(url + "timbr/api/query", headers = headers, json = post_data, verify = False)
+    response = requests.post(url + "timbr/api/query/", headers = headers, json = post_data, verify = False)
     response_data = response.json()
     if response_data['status'] == 'success':
         df = pandas.DataFrame(response_data['data'])
