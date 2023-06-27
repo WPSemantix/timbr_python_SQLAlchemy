a395518d3f0a91ef419f283b13b77c633e6cb7dd
Dor Eliyahu <dor@timbr.ai>
2023-06-27 16:06:52 +0300

    use as python package

---
+++ setup.py
@@ -0,0 +1,68 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+  long_description = fh.read()
+
+setuptools.setup(
+  name='pytimbrsqlalchemy',
+  version='1.0.0',
+  author='timbr',
+  author_email='contact@timbr.ai',
+  description='Timbr Python connector',
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url='https://github.com/DorEliyahu176/timbr_python_SQLAlchemy',
+  download_url = 'https://github.com/DorEliyahu176/timbr_python_SQLAlchemy/archive/refs/tags/v1.0.0.tar.gz',
+  project_urls={
+    "Bug Tracker": "https://github.com/DorEliyahu176/timbr_python_SQLAlchemy/issues"
+  },
+  license='MIT',
+  packages=['pytimbrsqlalchemy', 'TCLIService'],
+  install_requires=[
+    'future',
+    'python-dateutil',
+  ],
+  extras_require={
+    'hive': ['sasl>=0.2.1', 'thrift>=0.10.0', 'thrift_sasl>=0.1.0'],
+    'hive_pure_sasl': ['pure-sasl>=0.6.2', 'thrift>=0.10.0', 'thrift_sasl>=0.1.0'],
+    'sqlalchemy': ['sqlalchemy>=1.3.0'],
+    'kerberos': ['requests_kerberos>=0.12.0'],
+  },
+  package_data={},
+  keywords = [
+    'timbr',
+    'timbr-python',
+    'timbr-connector',
+    'python-connector',
+    'PyTimbr',
+    'pytimbr',
+    'py-timbr',
+    'Py-Timbr',
+    'pytimbrsqlalchemy',
+    'PyTimbrSqlalchemy',
+    'PyTimbrSQlalchemy',
+    'PyTimbrSQLalchemy',
+    'PyTimbrSQLAlchemy',
+    'Py-TimbrSQLAlchemy',
+    'py-timbrsqlalchemy',
+    'Py-Timbr-SQLAlchemy',
+    'py-timbr-sqlalchemy',
+  ],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ],
+  entry_points={
+    'sqlalchemy.dialects': [
+      'timbr = pytimbrsqlalchemy.sqlalchemy_timbr:TimbrDialect',
+      "timbr.http = pytimbrsqlalchemy.sqlalchemy_timbr:TimbrHTTPDialect",
+      "timbr.https = pytimbrsqlalchemy.sqlalchemy_timbr:TimbrHTTPSDialect",
+    ],
+  }
+)
