69be4fa3ed4ab45cb86aed95e866d00064469f39
Dor Eliyahu <dor@timbr.ai>
2023-07-31 17:45:02 +0300

    bumped patch version (1.0.1)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
   name='pytimbr_sqla',
-  version='1.0.0',
+  version='1.0.1',
   author='timbr',
   author_email='contact@timbr.ai',
   description='Timbr Python connector',
